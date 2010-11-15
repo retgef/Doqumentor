8bc973da9ea9a7999a5f209e2ce43d488d37791a
Murray Picton <murray@murraypicton.com>
2010-11-15 22:23:18 +0000

    Upgraded license to GPL3 and added license file and headers to all source files

--- jquery.js
+++ jquery.js
@@ -1,3 +1,19 @@
+/**
+	This file is part of Doqumentor.
+
+    Doqumentor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Doqumentor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Doqumentor.  If not, see <http://www.gnu.org/licenses/>.
+*/
 mydoc = $('#doqument');
 elements = mydoc.find('.constant, .function, .class, .method');
 elements.find('H2').addClass('ui-widget-header').addClass('ui-corner-all').css('padding-left', ' 0.5em').css('cursor', 'pointer');
